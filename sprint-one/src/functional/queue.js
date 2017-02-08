f1c2ebf1d4c45e16fc33b096a1a65ff7f19cb2f1
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-02-08 15:41:44 -0800

    Completed queue and passed all test cases.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var front = 0;
+  var back = 0;
 
   // Implement the methods below
 
+  // insert at the back
   someInstance.enqueue = function(value) {
+    storage[++back] = value;
   };
 
+  // take out the front
   someInstance.dequeue = function() {
+    if ((back - front) > 0) {
+      var temp = storage[++front];
+
+      delete storage[front];
+
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return back - front;
   };
 
   return someInstance;
