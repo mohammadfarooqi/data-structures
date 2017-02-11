861c5813bf9940ffa8cffad0f752be875d66ae2b
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-02-10 19:00:23 -0800

    Completed Binary Search Tree and passed all test cases.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,69 @@
 var BinarySearchTree = function(value) {
+  var bsTree = Object.create(binarySearchTreeMethods);
+
+  bsTree.value = value;
+  bsTree.left = null;
+  bsTree.right = null;
+
+  return bsTree;
+};
+
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+
+  var inner = function(node) {
+    if (node.value > value && node.left === null) {
+      node.left = newNode;
+      return;
+    } else if (node.value > value && node.left !== null) {
+      inner(node.left);
+    }
+
+    if (node.value < value && node.right === null) {
+      node.right = newNode;
+      return;
+    } else if (node.value < value && node.right !== null) {
+      inner(node.right);
+    }
+  };
+
+  inner(this);
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  var result = false;
+
+  var inner = function(node) {
+    if (node.value === value) {
+      result = true;
+    }
+
+    if (node.value > value && node.left !== null) {
+      inner(node.left);
+    } else if (node.value < value && node.right !== null) {
+      inner(node.right);
+    }
+  };
+
+  inner(this);
+
+  return result;
+};
+
+binarySearchTreeMethods.depthFirstLog = function(callback) {
+  var inner = function (node) {
+    callback(node.value);
+
+    if (node.left !== null) {
+      inner(node.left);
+    } else if (node.right !== null) {
+      inner(node.right);
+    }
+  };
+
+  inner(this);
 };
 
 
